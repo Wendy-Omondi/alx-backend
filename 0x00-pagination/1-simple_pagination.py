b07b52a6e10ed7d72666742956b501519ef4a135
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-08-09 18:38:36 +0000

    rewrite index_range

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-"""Simple Pagination"""
+""" Simple pagination """
 import csv
 import math
 from typing import List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """return a tuple of size two containing a start index
-       and an end index corresponding to the range of indexes
-       to return in a list for those particular pagination parameters.
-    """
-    end_index: int = page * page_size
-    start_index: int = end_index - page_size
-
-    return(start_index, end_index)
 
 
 class Server:
@@ -36,17 +25,24 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+        """return the appropriate page of the dataset
+           (i.e. the correct list of rows).
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
 
-def get_page(page: int = 1, page_size: int = 10) -> List[List]:
-    """ return the appropriate page of the dataset
-       (i.e. the correct list of rows).
+        ranges: Tuple = index_range(page, page_size)
+        pag: List = self.dataset()
+
+        return (pag[ranges[0]:ranges[1]])
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple of size two containing a start index
+       and an end index corresponding to the range of indexes
+       to return in a list for those particular pagination parameters.
     """
-    assert (page, int) and page > 0
-    assert (page_size, int) and page > 0
+    end_index: int = page * page_size
+    start_index: int = end_index - page_size
 
-    start_index: Tuple = index_range(page, page_size)
-    end_index: Tuple = index_range(page, page_size)
-    pages = self.dataset()
-
-    return pages[start_index:end_index]
+    return (start_index, end_index)
